fee410ea6c02a2d9a1f955de1fa8ee90f7e3d0ba
sj5lee <sj5lee1@gmail.com>
2024-09-19 14:06:08 +0900

    테스트 코드 추가 rag toggle 추가

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ st.sidebar.title("소개")
 st.sidebar.info(
     "이 앱은 RAG(검색 증강 생성) 시스템을 시연합니다. "
 )
+on=st.sidebar.toggle("RAG 기능 키기", True)
 # 데이터베이스에 문서 추가
-files=st.sidebar.file_uploader("RAG에 추가할 문서를 넣어주세요.", type=["txt", "pdf"], accept_multiple_files=True)
+files=st.sidebar.file_uploader("RAG에 추가할 문서를 넣어주세요.", type=["txt"], accept_multiple_files=True)
 # 사용자 입력
 question = st.text_input("질문하세요:")
 
@@ -21,7 +22,7 @@ if question:
             vectorstore = create_vectorstore(splits)
             
             # RAG 체인 생성
-            result = create_rag_chain(vectorstore, question)
+            result = create_rag_chain(vectorstore, question, on)
             
             st.subheader("답변:")
             st.write(result)
--- rag_functions.py
+++ rag_functions.py
@@ -3,11 +3,14 @@ import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import PromptTemplate
-from langchain_community.document_loaders import PyPDFDirectoryLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, BitsAndBytesConfig
 import torch
 import re
+from nltk.translate.bleu_score import sentence_bleu
+from rouge_score import rouge_scorer
+from io import StringIO
 
 # huggingface 모델 사용하기 위해 필요한 개인키
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
@@ -22,8 +25,8 @@ def load_docs(files):
     
     #streamlit ui를 통해 추가한 문서들을 data 디렉토리로 저장한다. 문서 내에 줄들 사이에 공백이 너무 크면 줄바꿈을 하나로 줄인다.
     for file in files:
-        savefile=open(file.name, "w", encoding='utf-8')
-        savefile.write(re.sub("(\r\n)+","\n",file.read().decode("utf-8")))
+        savefile=open(file.name, "w")
+        savefile.write(re.sub('\s\s+', '\n', file.read()))
 
         file.close()
         savefile.close()
@@ -33,7 +36,7 @@ def load_docs(files):
     for file in [f for f in os.listdir(os.curdir) if os.path.isfile(os.path.join(os.curdir, f))]:
         _, ext=file.split(".")
         if ext=="pdf":
-            loader=PyPDFDirectoryLoader(file)
+            loader=PyPDFLoader(file)
         elif ext=="txt":
             loader=TextLoader(file, encoding='utf-8')
         documents += loader.load()
@@ -46,7 +49,7 @@ def load_docs(files):
         chunk_overlap=int(512 / 10),
         add_start_index=True,
         strip_whitespace=True,
-        separators=["\n+"],
+        separators=["\n제"],
         is_separator_regex=True,
     )
 
@@ -60,7 +63,7 @@ def load_docs(files):
 def create_vectorstore(splits):
     embeddings = HuggingFaceEmbeddings(
         model_name="jhgan/ko-sroberta-multitask",
-        model_kwargs={'device':('cuda' if torch.cuda.is_available() else 'cpu')}, # Pass the model configuration options
+        model_kwargs={'device':('cuda:0' if torch.cuda.is_available() else 'cpu')}, # Pass the model configuration options
         encode_kwargs={'normalize_embeddings': True}, # Pass the encoding options
     )
     vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
@@ -68,7 +71,7 @@ def create_vectorstore(splits):
     return vectorstore
 
 #사용자한테 받은 질문으로 필요한 context를 찾은 후 프롬프트를 만들어 준비한 모델에 넣어 대답을 받는다.
-def create_rag_chain(vectorstore, question):
+def create_rag_chain(vectorstore, question, on):
     READER_MODEL_NAME = "maywell/Synatra-V0.1-7B-Instruct"
     # 크기와 복잡도를 줄이기 위해 파라미터 무게를 원래보다 더 작은 타입으로 바꾸는 quantization을 한다.
     bnb_config = BitsAndBytesConfig(
@@ -78,11 +81,12 @@ def create_rag_chain(vectorstore, question):
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
     if torch.cuda.is_available():
-        model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME, quantization_config=bnb_config)
+        model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME, quantization_config=bnb_config).to("cuda:0")
+        tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME).to("cuda:0")
     else:
         model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)
+        tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
 
-    tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
 
     #준비한 모델
     llm = pipeline(
@@ -97,27 +101,58 @@ def create_rag_chain(vectorstore, question):
     )
 
     # 사용할 prompt template
-    prompt_template = """아래의 문맥을 사용하여 질문에 답하십시오.
-    만약 답을 모른다면, 모른다고 말하고 답을 지어내지 마십시오.
-    최대한 세 문장으로 답하고 가능한 한 간결하게 유지하십시오.
-    {context}
-    질문: {question}
-    유용한 답변:"""
-    
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"],
-    )
+    print(f'on is {on}')
+    if on:
+        prompt_template = """아래의 문맥을 사용하여 질문에 답하십시오.
+        만약 답을 모른다면, 모른다고 말하고 답을 지어내지 마십시오.
+        최대한 세 문장으로 답하고 가능한 한 간결하게 유지하십시오.
+        {context}
+        질문: {question}
+        유용한 답변:"""
+        
+        PROMPT = PromptTemplate(
+            template=prompt_template, input_variables=["context", "question"],
+        )
+    else:
+        prompt_template = """질문에 답하십시오.
+        만약 답을 모른다면, 모른다고 말하고 답을 지어내지 마십시오.
+        최대한 세 문장으로 답하고 가능한 한 간결하게 유지하십시오.
+        질문: {question}
+        유용한 답변:"""
+        
+        PROMPT = PromptTemplate(
+            template=prompt_template, input_variables=["question"],
+        )
 
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
 
     #template에 정보와 질문을 넣은 후 모델에 던져 대답을 받는다.
-    prompt_inputs={
-        "context": format_docs(vectorstore.as_retriever().invoke(question)),
-        "question": question
-    }
+    if on:
+        prompt_inputs={
+            "context": format_docs(vectorstore.as_retriever().invoke(question)),
+            "question": question
+        }
+    else:
+        prompt_inputs={
+            "question": question
+        }
+
     final_prompt=PROMPT.invoke(prompt_inputs)
+    print(final_prompt)
     answer=llm(final_prompt.text)
     true_answer=answer[0]['generated_text'].split("질문:")[0].strip()
+
+    # 평가점수 얻기 위한 테스트용 코드
+    reference_str="""1. 이용하려는 토지의 도면
+    2. 매립폐기물의 종류ㆍ양 및 복토상태를 적은 서류
+    3. 지적도"""
+
+    print("BLEU score: {}".format(sentence_bleu(reference_str.split(), true_answer.split())))
+    print("ROUGE score:")
+    scorer=rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+    scores=scorer.score(reference_str, true_answer)
+    for key in scores:
+        print(f'\t{key}: {scores[key]}')
 
     return true_answer
